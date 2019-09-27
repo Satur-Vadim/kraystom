4d205381cf94f7e27cef9ab9d601ad93f3513f41
Satur-Vadim <satyrvadim4@gmail.com>
2019-09-27 14:23:57 +0300

    Create posts
    1) Create post page and archive page.
    2) Small corect styles
    3) Redefined tag select in sidebar

--- Blog.php
+++ Blog.php
@@ -6,8 +6,140 @@
 * @subpackage kraystom
 * @since kraystom 1.0
 */
- get_header(); 
- get_sidebar();
- ?>
+get_header();
+?>
+<div class="container">
+	<div class="row posts-page">
+		<div class="col-lg-3 post-sidebar"><?php echo get_sidebar(); ?></div>
+		<div class="col-lg-8 post-content">
+			<div class="posts-beforecontent">
+				<div class="posts-beforecontent__title">
+					<div class="posts-beforecontent__title-icon"><i class="fal fa-paper-plane"></i></div>
+					<div class="posts-beforecontent__title-text">
+						Сервис быстро растет. Постоянно появляются новые обзоры и проекты.<br>
+						Подпишитесь, чтобы не пропустить самое интересное.
+					</div>
+				</div>
+				<div class="posts-beforecontent__form"></div>
+			</div>
+			<div class="posts-container">
+			<?php
+			global $wp_query;
+			$wp_query = new WP_Query(array(
+				'posts_per_page' => '8',
+				'paged' => get_query_var('paged') ?: 1 // страница пагинации
+			));
 
-<?php get_footer() ?>
+			while( have_posts() ){
+				the_post();?>
+   			<div class="post-item">
+   				<div class="post-item__content">
+	   				<div class="post-item__top-block">
+	   					<div class="post-item__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+						<div class="post-item__date"><i class="fal fa-calendar"></i><span class="post-date"><?php the_time('j F Y'); ?></span></div>
+	   				</div>
+	   				<div class="post-item__text">
+	   					<?php the_excerpt(); ?>
+	   				</div>
+   				</div>
+   				<div class="post-item__img">
+   					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+   				</div>
+			</div>
+     		<?php };
+     		$pag = array(
+     			'end_size'     => 3,
+     			'prev_text'    => '<i class="far fa-chevron-left"></i>',
+				'next_text'    => '<i class="far fa-chevron-right"></i>',
+     		);
+     		the_posts_pagination($pag);
+     		wp_reset_query(); ?>	
+   			</div>
+		</div>
+		<div class="col-12">
+			<?php echo do_shortcode('[recent_products per_page="3" columns="3"]'); ?>	
+		</div>
+	</div>
+</div>
+<div id="seo-text">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<p>Вы находитесь на сервисе KRAYSTOM. Все лучшие компании Томска в одном месте. </p>
+				<p>Здесь Вам помогут подобрать нужный проект и надежного проверенного исполнителя полностью бесплатно. Без скрытых платежей и звездочек. По цене застройщика, без наценок. Больше ненужно перелопачивать горы информации в поисках надежного исполнителя  и лучшей цены. 
+				Позвоните в Крайстом сейчас.</p>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+
+get_footer();?>
+<script>
+  ( function( $ ) {
+    $(document).ready(function(){
+    $('.select__item').eq(0).remove();
+    });
+  })( jQuery );
+</script>
+<script>
+	( function( $ ) {
+$('.postform').each(function() {
+  var $this = $(this),
+    selectOption = $this.find('option'),
+    selectOptionLength = selectOption.length,
+    selectedOption = selectOption.filter(':selected'),
+    dur = 500;
+
+  $this.hide();
+  $this.wrap('<div class="select"></div>');
+  $('<div>', {
+    class: 'select__gap',
+    text: selectOption.eq(0).text()
+  }).insertAfter($this);
+
+  var selectGap = $this.next('.select__gap'),
+    caret = selectGap.find('.caret');
+  $('<ul>', {
+    class: 'select__list'
+  }).insertAfter(selectGap);
+
+  var selectList = selectGap.next('.select__list');
+  // Add li - option items
+  for (var i = 0; i < selectOptionLength; i++) {
+    $('<li>', {
+        class: 'select__item',
+        html: $('<span>', {
+          text: selectOption.eq(i).text()
+        })
+      })
+      .attr('data-value', selectOption.eq(i).val())
+      .appendTo(selectList);
+  }
+  var selectItem = selectList.find('li');
+
+  selectList.slideUp(0);
+  selectGap.on('click', function() {
+    if (!$(this).hasClass('on')) {
+      $(this).addClass('on');
+      selectList.slideDown(dur);
+
+      selectItem.on('click', function() {
+        var chooseItem = $(this).data('value');
+
+        $('select').val(chooseItem).attr('selected', 'selected');
+        selectGap.text($(this).find('span').text());
+
+        selectList.slideUp(dur);
+        selectGap.removeClass('on');
+      });
+
+    } else {
+      $(this).removeClass('on');
+      selectList.slideUp(dur);
+    }
+  });
+});
+	$(".select").after("<div class='widget-btn'><button class='btn' type='submit'>Показать статьи</button></div>");
+})( jQuery );
+</script>
--- archive.php
+++ archive.php
@@ -11,47 +11,114 @@ get_header();
 ?>
 <div class="container">
 	<div class="row posts-page">
-		<div class="col-lg-3"><?php echo get_sidebar(); ?></div>
-		<div class="col-lg-8">
-			<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			//get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-		</div>
+		<div class="col-lg-3 post-sidebar"><?php echo get_sidebar(); ?></div>
+		<div class="col-lg-8 post-content">
+      <div class="posts-beforecontent">
+        <div class="posts-beforecontent__title">
+          <div class="posts-beforecontent__title-icon"><i class="fal fa-paper-plane"></i></div>
+          <div class="posts-beforecontent__title-text">
+            Сервис быстро растет. Постоянно появляются новые обзоры и проекты.<br>
+            Подпишитесь, чтобы не пропустить самое интересное.
+          </div>
+        </div>
+        <div class="posts-beforecontent__form"></div>
+      </div>
+      <div class="posts-container">
+      <?php
+      while( have_posts() ){
+        the_post();?>
+        <div class="post-item">
+          <div class="post-item__content">
+            <div class="post-item__top-block">
+              <div class="post-item__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+            <div class="post-item__date"><i class="fal fa-calendar"></i><span class="post-date"><?php the_time('j F Y'); ?></span></div>
+            </div>
+            <div class="post-item__text">
+              <?php the_excerpt(); ?>
+            </div>
+          </div>
+          <div class="post-item__img">
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a>
+          </div>
+      </div>
+        <?php };
+        the_posts_pagination();
+        wp_reset_query(); ?>  
+        </div>
+    </div>
 	</div>
 </div>
 		
 
 <?php
 
-get_footer();
+get_footer();?>
+<script>
+  ( function( $ ) {
+    $(document).ready(function(){
+    $('.select__item').eq(0).children().text('Все темы');
+    });
+  })( jQuery );
+</script>
+<script>
+  ( function( $ ) {
+$('.postform').each(function() {
+  var $this = $(this),
+    selectOption = $this.find('option'),
+    selectOptionLength = selectOption.length,
+    selectedOption = selectOption.filter(':selected'),
+    selected = $("#cat :selected").text();
+    dur = 500;
+
+  $this.hide();
+  $this.wrap('<div class="select"></div>');
+  $('<div>', {
+    class: 'select__gap',
+    text: selected
+  }).insertAfter($this);
+
+  var selectGap = $this.next('.select__gap'),
+    caret = selectGap.find('.caret');
+  $('<ul>', {
+    class: 'select__list'
+  }).insertAfter(selectGap);
+
+  var selectList = selectGap.next('.select__list');
+  // Add li - option items
+  for (var i = 0; i < selectOptionLength; i++) {
+    $('<li>', {
+        class: 'select__item',
+        html: $('<span>', {
+          text: selectOption.eq(i).text()
+        })
+      })
+      .attr('data-value', selectOption.eq(i).val())
+      .appendTo(selectList);
+  }
+  var selectItem = selectList.find('li');
+
+  selectList.slideUp(0);
+  selectGap.on('click', function() {
+    if (!$(this).hasClass('on')) {
+      $(this).addClass('on');
+      selectList.slideDown(dur);
+
+      selectItem.on('click', function() {
+        var chooseItem = $(this).data('value');
+
+        $('select').val(chooseItem).attr('selected', 'selected');
+        selectGap.text($(this).find('span').text());
+
+        selectList.slideUp(dur);
+        selectGap.removeClass('on');
+      });
+
+    } else {
+      $(this).removeClass('on');
+      selectList.slideUp(dur);
+    }
+  });
+});
+  $(".select").after("<div class='widget-btn'><button class='btn' type='submit'>Показать статьи</button></div>");
+})( jQuery );
+</script>
